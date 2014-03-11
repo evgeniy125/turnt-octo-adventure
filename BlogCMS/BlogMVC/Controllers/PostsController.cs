34326b6d4ed3fee779b97749677d22b40c95ffb3
evgeniy125 <evgenij_telkov@mail.ru>
2014-03-11 16:12:46 +0700

    Initial Commit

---
+++ BlogCMS/BlogMVC/Controllers/PostsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BlogMVC.Models;
+using Microsoft.AspNet.Identity;
+
+namespace BlogMVC.Controllers
+{
+    [Authorize]
+    public class PostsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index([Bind(Prefix = "id")] string userId)
+        {
+            var user = db.Users.Find(userId);
+            if (user != null)
+                return View(user);
+            return HttpNotFound();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        [Authorize(Roles = "admin,writer")]
+        public ActionResult Create(string userId)
+        {
+            if (User.Identity.GetUserId().ToString() != userId)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin,writer")]
+        public ActionResult Create(Post post)
+        {
+            if (User.Identity.GetUserId().ToString() != post.UserId)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index", new { id = post.UserId });
+            }
+
+            return View(post);
+        }
+
+        [Authorize(Roles="admin,writer")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            if (User.IsInRole("writer") && post.User.Id != User.Identity.GetUserId())
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            return View(post);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin,writer")]
+        public ActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                if (User.IsInRole("writer") && post.UserId != User.Identity.GetUserId())
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", new {id = post.UserId});
+            }
+            return View(post);
+        }
+
+        [Authorize(Roles = "admin,writer")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            if (User.IsInRole("writer") && post.UserId != User.Identity.GetUserId())
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            return View(post);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin,writer")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Post post = db.Posts.Find(id);
+            if (User.IsInRole("writer") && post.UserId != User.Identity.GetUserId())
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index", new {id = post.UserId});
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BlogCMS/BlogMVC/Controllers/UserController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BlogMVC.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System.Web.Security;
+using PagedList;
+
+namespace BlogMVC.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index(int page = 1)
+        {
+            return View(db.Users.OrderBy(u => u.UserName).ToPagedList(page, 5));
+        }
+
+        [Authorize(Roles="admin")]
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+
+            var model = new UserRoleViewModel(db.Roles.ToList()) { UserId = user.Id, UserName = user.UserName,
+                SelectedRoleId = user.Roles.Single().Role.Id };
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit(UserRoleViewModel viewModel)
+        {
+            var user = db.Users.Find(viewModel.UserId);
+
+            if (user.Roles.Count != 1)
+                return null;
+            var role = db.Roles.Where(r => r.Id == viewModel.SelectedRoleId).Single();
+            user.Roles.Clear();
+            user.Roles.Add(new IdentityUserRole { UserId = user.Id, RoleId = role.Id });
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(viewModel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
