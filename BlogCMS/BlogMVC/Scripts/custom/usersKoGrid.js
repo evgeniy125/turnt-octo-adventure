c1e7ab8ff1348380ea86ef56f3d44d5ed7bcd5ca
evgeniy125 <evgenij_telkov@mail.ru>
2014-04-11 16:27:41 +0700

    Knockout grid second version ready

--- BlogCMS/BlogMVC/Scripts/custom/usersKoGrid.js
+++ BlogCMS/BlogMVC/Scripts/custom/usersKoGrid.js
@@ -4,14 +4,14 @@
     self.rows = ko.observableArray([]);
 
     self.filters = [
-    { title: 'User name', filterPropertyName: "UserName", param: ko.observable("") },
-    { title: 'Create date', filterPropertyName: "CreateDate", param: ko.observable("") },
-    { title: 'Role name', filterPropertyName: "RoleName", param: ko.observable("") }];
+    { title: 'User name', filterPropertyName: "UserName", param: ko.observable("").extend({ rateLimit: { method: "notifyWhenChangesStop", timeout: 500} }) },
+    { title: 'Create date', filterPropertyName: "CreateDate", param: ko.observable("").extend({ rateLimit: { method: "notifyWhenChangesStop", timeout: 500 } }) },
+    { title: 'Role name', filterPropertyName: "RoleName", param: ko.observable("").extend({ rateLimit: { method: "notifyWhenChangesStop", timeout: 500 } }) }];
     self.headers = [
-    { title: 'User name', sortPropertyName: 'UserName', asc: ko.observable(true), active: ko.observable(false) },
-    { title: 'Create date', sortPropertyName: 'CreateDate', asc: ko.observable(true), active: ko.observable(false) },
-    { title: 'Role name', sortPropertyName: 'RoleName', asc: ko.observable(true), active: ko.observable(false) },
-    { title: 'Actions', sortPropertyName: null, asc: ko.observable(true), active: ko.observable(false) }];
+    { title: 'USER NAME', sortPropertyName: 'UserName', asc: ko.observable(true), active: ko.observable(false) },
+    { title: 'CREATE DATE', sortPropertyName: 'CreateDate', asc: ko.observable(true), active: ko.observable(false) },
+    { title: 'ROLE NAME', sortPropertyName: 'RoleName', asc: ko.observable(true), active: ko.observable(false) },
+    { title: 'ACTIONS', sortPropertyName: null, asc: ko.observable(true), active: ko.observable(false) }];
 
     //Sorting and filtering
 
@@ -25,14 +25,38 @@
         header.active(true);
 
         var prop = header.sortPropertyName;
-        var ascSort = function (a, b) { return a[prop] < b[prop] ? -1 : a[prop] > b[prop] ? 1 : 0 };
-        var descSort = function (a, b) { return a[prop] > b[prop] ? -1 : a[prop] < b[prop] ? 1 : 0 };
+        var ascSort = function (a, b) {
+            if (ko.isObservable(a[prop]))
+                return a[prop]() < b[prop]() ? -1 : a[prop]() > b[prop]() ? 1 : 0;
+            else {
+                return a[prop] < b[prop] ? -1 : a[prop] > b[prop] ? 1 : 0;
+            }
+        };
+        var descSort = function (a, b) {
+            if (ko.isObservable(a[prop]))
+                return a[prop]() > b[prop]() ? -1 : a[prop]() < b[prop]() ? 1 : 0;
+            else {
+                return a[prop] > b[prop] ? -1 : a[prop] < b[prop] ? 1 : 0;
+            }
+        };
         var sortFunc = header.asc() ? ascSort : descSort;
 
         self.activeSort(sortFunc);
     };
 
     self.activeSort = ko.observable(function () { return 0; });
+
+
+    self.value = ko.observable();
+
+    self.delayedValue = ko.computed(self.value).extend({ rateLimit: { method: "notifyWhenChangesStop", timeout: 400 } });
+
+
+
+    //self.bindParam = function (data, event) {
+    //    var handler = function () { $(event.target).trigger('change'); }
+    //    setTimeout(handler, 500);
+    //};
 
     self.filteredRows = ko.computed(function () {
         var temp = self.rows();
@@ -41,22 +65,24 @@
             var filter = self.filters[i];
             if (filter.param() != "") {
                 temp = ko.utils.arrayFilter(temp, function (item) {
-
-                    //Поменять для того, чтобы исчезала из отфильтрованного
-                    //списка только после изменения и новой фильтрации
                     if (ko.isObservable(item[filter.filterPropertyName])) {
-                        return item[filter.filterPropertyName]() == filter.param();
+                        return item[filter.filterPropertyName]().substr(0, filter.param().length) == filter.param();
                     }
                     else {
-                        return item[filter.filterPropertyName] == filter.param();
+                        return item[filter.filterPropertyName].substr(0, filter.param().length) == filter.param();
                     }
-                    //return item[filter.filterPropertyName] == filter.param();
                 });
             }
         }
         return temp.sort(self.activeSort());
     });
 
+
+    //Read
+
+    self.read = function (data) {
+        document.location.href = '../../posts/index/' + data.UserId;
+    };
 
     //Select
 
@@ -80,12 +106,7 @@
                 alert("success");
                 for (var i = 0; i < self.roles.length; i++) {
                     if (self.roles[i]["Id"] == data["RoleId"]()) {
-
-                        //Поменять для того, чтобы исчезала из отфильтрованного
-                        //списка только после изменения и новой фильтрации
                         data["RoleName"](self.roles[i]["Name"]);
-                        //data["RoleName"] = self.roles[i]["Name"];
-
                     }
                 }
                 data["SaveRequired"](false);
@@ -95,6 +116,7 @@
             }
         })
     };
+
 
     self.roles = [];
 
@@ -115,13 +137,9 @@
     $.getJSON(itemsUrl, function (data) {
         $.each(data, function (key, val) {
             val["RoleId"] = ko.observable(val["RoleId"]);
-
-            //Закомментировать для того, чтобы исчезала из отфильтрованного
-            //списка только после изменения и новой фильтрации
             val["RoleName"] = ko.observable(val["RoleName"]);
-
+            val["CreateDate"] = new Date(parseInt(val["CreateDate"].substr(6))).toDateString();
             val["SaveRequired"] = ko.observable(false);
-            self.rows.push(val);
         });
         self.rows(data);
     });
